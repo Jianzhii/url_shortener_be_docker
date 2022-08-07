1578b2eb9c7dd20a2cbc3a3a60591a545e955453
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2022-08-08 01:11:52 +0800

    add config and db initialisation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UrlModule } from './url/url.module';
+import * as Joi from '@hapi/joi';
+import * as path from 'path';
+console.log(__dirname);
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().required(),
+                NODE_PORT: Joi.number().required(),
+                DATABASE_HOST: Joi.string().required(),
+                DATABASE_USER: Joi.string().required(),
+                DATABASE_PASSWORD: Joi.string().required(),
+                DATABASE_PORT: Joi.number().required().default(3306),
+                DATABASE: Joi.string(),
+            }),
+        }),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.DATABASE_HOST,
+            port: Number(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE,
+            entities: [path.join(__dirname, '*.entity.js')],
+            synchronize: false,
+        }),
+        UrlModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
